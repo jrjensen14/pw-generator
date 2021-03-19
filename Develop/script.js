86adcddc04ffa9e0fccbab66c1708b7b0f78bc36
JRJ <jjsocc171@msn.com>
2021-03-19 12:54:23 -0500

    updating

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,8 @@
 //password is either displayed in an alert or written to the page
 
 
-var uppercaseChar= ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"];
+var upperCaseChar= ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"];
 var numberChar= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",];
 var specialChar= ["!", "@", "#", "$", "%", "^", "&", "*"];
     console.log(uppercaseChar, lowercaseChar, numberChar, specialChar);
