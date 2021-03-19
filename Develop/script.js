a4ecc1643472f602a181336ec449f96f3a3644af
JRJ <jjsocc171@msn.com>
2021-03-19 13:51:38 -0500

    updating functions

--- Develop/script.js
+++ Develop/script.js
@@ -7,20 +7,23 @@
 //password is either displayed in an alert or written to the page
 
 
-var upperCaseChar= ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCaseChar= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"];
-var numberChar= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",];
-var specialChar= ["!", "@", "#", "$", "%", "^", "&", "*"];
-    console.log(uppercaseChar, lowercaseChar, numberChar, specialChar);
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "w", "y", "z"];
+var numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
+console.log(uppercaseChar, lowercaseChar, numberChar, specialChar);
 
-function generatePassword () {
+function passwordOptions() {
   //Lenght 8-128 characters
-  var charLength = window.prompt("Pick the length of the password between 8-128 characters.");
+  var charLength = parseInt(
+    prompt("Pick the length of the password between 8-128 characters.")
+  );
 
-  while (charLength <= 7 || charLength >= 129) { 
+  while (charLength <= 7 || charLength >= 129) {
     window.alert("Please pick a number in the specified range.");
-    return charLength; }
-    console.log(charLength);
+    return charLength;
+  }
+  console.log(charLength);
 
 
   // Prompts //
@@ -28,42 +31,77 @@ function generatePassword () {
   var confirmLowercase = window.confirm("Would you like to use lowercase letters? Choose 'ok' for yes or 'cancel' for no");
   var confirmNumber = window.confirm("Would you like ot use numbers? Choose 'ok' for yes or 'cancel' for no");
   var confrimSpecial = window.confirm("Would you like to use special charaters? Choose 'ok' for yes or 'cancel' for no");
-    console.log(confirmUppercase, confirmLowercase, confirmNumber, confrimSpecial);
+  console.log(confirmUppercase, confirmLowercase, confirmNumber, confrimSpecial);
 
   // Alert if none of the charecter types are clikced.  
-  while (confirmUppercase == false && confirmLowercase == false && confirmNumber == false && confrimSpecial == false) {
+  while (
+    confirmUppercase == false && 
+    confirmLowercase == false && 
+    confirmNumber == false && 
+    confrimSpecial == false
+    ) {
     window.alert("Please pick at least at least ONE character type.")
-    return confirmUppercase; }
-  
+    return confirmUppercase;
+  }
+
 
   // characters return "true" or yes
-  pwOptions = []
-
-  if (confirmUppercase == ture) {
-    pwOptions = pwOptions + uppercaseChar
-  }
-  
-  if (confirmLowercase == true) {
-    pwOptions = pwOptions + lowercaseChar
-  }
-
-  if (confirmNumber == true) {
-    pwOptions = pwOptions + numberChar
-  }
-  
-  if (confrimSpecial == true) {
-    pwOptions = pwOptions + specialChar
-  }
-    console.log(pwOptions);
-
-  var pwRandom = ""
-
-  for (var i = 0; i < charLength; i++) {
-    pwRandom = pwRandom + pwOptions(Math.floor(Math.random() * pwOptions.length))
-    console.log(pwRandom);
-  }
-  return pwRandom;
+ var pwOptions = {
+   length: length,
+   confirmUppercase: confirmUppercase,
+   confirmLowercase: confirmLowercase,
+   confirmNumber:confirmNumber,
+   confrimSpecial: confrimSpecial
+ }
+ return pwOptions;
 }
+
+function generateRandomPw(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElemant = arr[randIndex];
+
+  return randElemant;
+}
+
+function generatePassword() {
+  var options = generatePasswordOpt();
+  var result = [];
+  var possibleChar = [];
+  var guaranteedChar = [];
+
+  if (options.confirmUppercase) {
+    possibleChar = possibleChar.concat(upperCaseChar);
+    guaranteedChar.push(generateRandomPw(upperCaseChar));
+  }
+
+  if (options.confirmLowercase) {
+    possibleChar = possibleChar.concat(lowerCaseChar);
+    guaranteedChar.push(generateRandomPw(lowerCaseChar));
+  }
+  
+  if (options.confirmNumber) {
+    possibleChar = possibleChar.concat(numberChar);
+    guaranteedChar.push(generateRandomPw(numberChar));
+  }
+
+  if (options.confrimSpecial) {
+    possibleChar = possibleChar.concat(specialChar);
+    guaranteedChar.push(generateRandomPw(specialChar));
+  }
+
+  for (var i = 0; i < options.length; i++) {
+    var possibleChar = generateRandomPw(possibleChar);
+    result.push(possibleChar);
+  }
+
+  for (var i = 0; i < guaranteedChar.length; i++) {
+    result[i] = guaranteedChar[i];
+    
+  }
+  return result.join(''); 
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -74,8 +112,19 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+//   for (var i = 0; i < charLength; i++) {
+//     pwRandom = pwRandom + pwOptions(Math.floor(Math.random() * pwOptions.length))
+//     console.log(pwRandom);
+//   }
+//   return pwRandom;
+// }
